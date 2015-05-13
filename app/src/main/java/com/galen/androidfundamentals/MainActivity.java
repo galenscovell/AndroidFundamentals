35f6c24118940942e071f9214df3106c7cdc139b
Galen Scovell <galen.scovell@gmail.com>
2015-05-12 20:59:31 -0700

    Activity layouts, input, Intents, String resources

--- app/src/main/java/com/galen/androidfundamentals/MainActivity.java
+++ app/src/main/java/com/galen/androidfundamentals/MainActivity.java
@@ -1,12 +1,38 @@
+
+/**
+ * MAINACTIVITY CLASS
+ * Handles functions contained within the MainActivity activity.
+ */
+
 package com.galen.androidfundamentals;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+    // Define key for Intent's extra using package prefix for uniqueness
+    public final static String EXTRA_MESSAGE = "com.galen.androidFundamentals.MESSAGE";
+
+
+    // Custom method for sending message on button press
+    public void sendMessage(View view) {
+        // Create Intent for binding separate activities
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        // Create EditText element from edit_message ID
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        // Assign text to local variable
+        String message = editText.getText().toString();
+        // Add text value to Intent via putExtra(NAME, value)
+        intent.putExtra(EXTRA_MESSAGE, message);
+        // Finish Intent by calling activity start
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
